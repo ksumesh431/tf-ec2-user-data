914ce8dd5fcaaad2ca00bdc8cf24a3a5f9a419e6
Sumesh Kumar Panda <sumesh42069@gmail.com>
2022-04-15 23:37:23 +0530

    Add files via upload
    added provisioners

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,28 @@
 resource "aws_instance" "myec2vm" {
   ami                    = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
-  user_data              = file("${path.module}/app1-install.sh")
+  user_data              = file("app1-install.sh")
   key_name               = var.instance_keypair
   vpc_security_group_ids = [aws_security_group.ssh.id, aws_security_group.http.id]
   tags = {
     "Name" = "Testing EC2 with security groups"
   }
+  # provisioner "file" {
+  #   source      = "D:/Cloud Courses/terraform/ec2-sg/app1-install.sh"
+  #   destination = "/home/ec2-user/app1-install.sh"
+  # }
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "chmod +x app1-install.sh",
+  #     "sudo ./app1-install.sh"
+  #   ]
+  # }
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file("D:/Cloud Courses/aws course/key pair file/EC2Tutorial.pem")
+    timeout     = "4m"
+  }
+
 }
--- security-groups.tf
+++ security-groups.tf
@@ -2,24 +2,37 @@ resource "aws_security_group" "ssh" {
   name        = "ssh"
   description = "security group to allow ssh"
   ingress {
-    description      = "allow port 22"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    description = "allow port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+
 
 resource "aws_security_group" "http" {
   name        = "http"
   description = "security group to allow http requests"
   ingress {
-    description      = "allow port 80"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    description = "allow port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
